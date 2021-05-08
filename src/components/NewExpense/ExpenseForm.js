5bacac76b6d727ee5f629aa519e620917eb301fe
Soykan <mehmetsoykanmutlu@gmail.com>
2021-05-08 18:26:38 +0300

    added submitHandler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,10 +6,20 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-
+ /* useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+*/
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
     console.log(enteredTitle);
+   /* //setUserUInput({
+     ...userInput
+     eneterdTitle: event.target.value,
+    })
+    */
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
@@ -21,8 +31,19 @@ const ExpenseForm = () => {
     console.log(enteredDate);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+  }
+
   return (
-    <form>
+    <form onSubmit= {submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
